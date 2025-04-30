d968385e45bc1fdaeb460f25165394fe12f16078
brunofhorn <b.fernandeshorn@gmail.com>
2025-04-30 00:16:22 -0300

    feat: add solid principles to repositories and use-cases

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,6 @@
-import { registerUseCase } from "@/use-cases/register";
+import { PrismaUserRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
+import { RegisterUseCase } from "@/use-cases/register";
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
 
@@ -12,9 +14,20 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body);
 
   try {
-    await registerUseCase({ name, email, password });
+    const usersRepository = new PrismaUserRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    });
   } catch (error) {
-    return reply.status(409).send();
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message });
+    }
+
+    throw error;
   }
 
   return reply.status(201).send();
